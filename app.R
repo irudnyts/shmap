33fd7dfc3c23d9d087fd1c83514803ccd226912e
Iegor Rudnytskyi <yegor.rudnytskiy@gmail.com>
2022-04-18 17:38:32 +0200

    Move CSS to the separate file

--- app.R
+++ app.R
@@ -10,7 +10,7 @@ dameges <- tibble(
 )
 
 ui <- fillPage(
-    tags$style(type = "text/css", "html, body {width:100%; height:100%}"),
+    includeCSS("www/style.css"),
     leafletOutput("map", width = "100%", height = "100%")
     
 )
