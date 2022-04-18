a4ad112dad517a0d85c08a7fdf305e53ea3ef3ab
Iegor Rudnytskyi <yegor.rudnytskiy@gmail.com>
2022-04-18 19:55:54 +0200

    Add experimental HTML render in the pop-up

--- app.R
+++ app.R
@@ -9,6 +9,20 @@ dameges <- tibble(
     longitude = c(36.32800614300417, 36.251797616496205)
 )
 
+dameges[1, 2] <- '
+<div class="scroll-container">
+  <div class="gridscroll">
+      <img src="/schools/17/1.jpg">
+      <img src="/schools/17/2.jpg">
+      <img src="/schools/17/3.jpg">
+      <img src="/schools/17/4.jpg">
+      <img src="/schools/17/5.jpg">
+      <img src="/schools/17/6.jpg">
+      <img src="/schools/17/7.jpg">
+  </div>
+</div>
+'
+
 ui <- fillPage(
     includeCSS("www/style.css"),
     leafletOutput("map", width = "100%", height = "100%")
