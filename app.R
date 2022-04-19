5aa593510e172bdb31f5ccb8ff947d1ef042b5ac
Iegor Rudnytskyi <yegor.rudnytskiy@gmail.com>
2022-04-19 10:55:45 +0200

    Enable authentification for the app

--- app.R
+++ app.R
@@ -1,8 +1,10 @@
 library(shiny)
 library(leaflet)
 library(tidyverse)
+library(shinymanager)
 
 source("helpers.R")
+source("creditentials.R")
 
 dameges <- get_dameges()
 
@@ -13,10 +15,13 @@ dameges <- left_join(dameges, images)
 ui <- fillPage(
   includeCSS("www/style.css"),
   leafletOutput("map", width = "100%", height = "100%")
-  
-)
+) %>% secure_app()
 
 server <- function(input, output, session) {
+  
+  res_auth <- secure_server(
+    check_credentials = check_credentials(credentials)
+  )
   
   output$map <- renderLeaflet({
     leaflet() %>%
